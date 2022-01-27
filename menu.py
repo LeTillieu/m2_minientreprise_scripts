0d728fa6f87a892a3c8467720ac8ea41f23ea4fb
leotll <leo.tillieu@isen.yncrea.fr>
2022-01-27 04:36:30 +0100

    Push initialisation de la structure des fichiers

--- menu.py
+++ menu.py
@@ -6,20 +6,24 @@ def getStarted():
 	print("Bienvenue sur le gestionnaire du jeu serieux 'Cyber Dem'")
 	print("Quelle activité voulez-vous lancer ?")
 
-	chosen_dir=[]
 
-	for root, dirs, filenames in os.walk("."):
-		print(dirs)
-		for id,dir in enumerate(dirs[::-1]) :
-			chosen_dir.append(dir)
-			print(id," : ",dir)
+	for root, dirs, filenames in os.walk("./activite"):
+	
+		filenames=list(filter(lambda x: x.split('.')[1]=='json',filenames))
+		
+		for id,files in enumerate(filenames) :
+			print(id," : ",files)
 		break
+		
 	choice=int(input("Votre choix (entier seulement) : "))
 
-
-	confirm=input(f"Etes-vous sûr de vouloir lancer l'activité {chosen_dir[choice]} ? (O/N) ")
+	confirm=input(f"Etes-vous sûr de vouloir lancer l'activité {files[choice]} ? (O/N) ")
+	
 	if confirm=="O" or confirm =="o":
-		os.system(f"./{chosen_dir[choice]}/main.sh")
+	
+		os.system(f"python /activite/parse.py {files[choice]} ")
+		
 	else:
 		getStarted()
+		
 getStarted()
