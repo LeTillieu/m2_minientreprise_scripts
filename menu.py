6a53fdbbe53476612116a4e29ef11308c563d052
leotll <leo.tillieu@isen.yncrea.fr>
2022-03-03 14:46:30 +0100

    yaml first try [parse.py not working]

--- menu.py
+++ menu.py
@@ -9,19 +9,20 @@ def getStarted():
 
 	for root, dirs, filenames in os.walk("./activite"):
 	
-		filenames=list(filter(lambda x: x.split('.')[1]=='json',filenames))
+		filenames=list(filter(lambda x: x.split('.')[1]=='yml',filenames))
 		
 		for id,files in enumerate(filenames) :
+			
 			print(id," : ",files)
 		break
 		
 	choice=int(input("Votre choix (entier seulement) : "))
-
-	confirm=input(f"Etes-vous sûr de vouloir lancer l'activité {files[choice]} ? (O/N) ")
+	print(filenames[choice])
+	confirm=input(f"Etes-vous sûr de vouloir lancer l'activité {filenames[choice]} ? (O/N) ")
 	
 	if confirm=="O" or confirm =="o":
-	
-		os.system(f"python /activite/parse.py {files[choice]} ")
+		
+		os.system(f"python parse.py {filenames[choice]} ")
 		
 	else:
 		getStarted()
--- parse.py
+++ parse.py
@@ -1,2 +1,14 @@
-def __main__() :
+import sys
+import yaml
+
+args=sys.argv
+
+activity_name= args[1]
+path='./activite/'+activity_name
+
+config_file=open(path)
+
+parsed_config_file=yaml.load(config_file,Loader=yaml.FullLoader)
+
+print(parsed_config_file)
 	
