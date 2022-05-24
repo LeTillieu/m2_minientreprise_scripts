1a9d21081f7438d8fe5736b55f8adb76c7d5a406
Lilogan <lilianhouplin@hotmail.fr>
2022-05-24 18:05:11 +0200

    [Action] - Updating parser - Using a queue - Updating parser - Updating Class

--- action.py
+++ action.py
@@ -1,29 +1,54 @@
-class Action:
-  def __init__(self, name):
-    self.name = name
+import time
 
-  def run():
+class Action:
+  def __init__(self, action):
+    self.name = action['name']
+
+  def run(self):
     pass
 
 
 class Script(Action):
-  def __init__(self, name, script):
-    super().__init__(name)
-    self.script = script
+  def __init__(self, action):
+    super().__init__(action)
+    self.script = action['script']
+    self.hosts = action['hosts']
+    self.args = action['args']
 
+  def run(self):
+    params = ''
+    if self.args:
+      for param in self.args:
+        params+='-'+param['name']+' '+str(param['value'])+' '
+
+    for host in self.hosts:
+      if host['name'] == 'local':
+        print(f'./scripts/{self.script} {param}')
+      else :
+        print(f'{host.name} : ./scripts/{self.script} {param}')
 
 class Wait(Action):
-  def __init__(self, name, time):
-    super().__init__(name)
-    self.time = time
+  def __init__(self, action):
+    super().__init__(action)
+    self.text = action['text']
+    self.time = action['time'] if 'time' in action else None
+
+  def run(self):
+    if self.time:
+      print(self.text)
+      time.sleep(self.time)
+    else :
+      choice = input(self.text)
 
 
 class Hook(Action):
   running = []
 
-  def __init__(self, name, route):
-    super().__init__(name)
-    self.route = route
+  def __init__(self, action):
+    super().__init__(action)
+    self.route = action['route']
+
+  def run(self):
     Hook.running.append(self)
 
   def __del__(self):
--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from action import Hook, Script, Wait
 from threading import Thread
-import sys
+from sys import argv
 import logging
 import click
 import yaml
@@ -38,89 +38,27 @@ if __name__ == '__main__':
   kwargs = {'host': '0.0.0.0', 'port': '5443', 'threaded': True, 'use_reloader': False, 'debug': False}
   flaskThread = Thread(target=app.run, daemon=True, kwargs=kwargs,).start()
 
+# activity_name= argv[1]
+# path='./activite/'+activity_name
 
+path='./activite/A_phishing.yml'
+file=open(path)
+data=yaml.load(file,Loader=yaml.FullLoader)
+actions = []
 
-def script_module(action):
-
-	action_name=action["name"]
-	script_name=action['script']
-	hosts=action['hosts']
-	script_args=action['args']
-	params=""
-
-	if(script_args):
-		for param in script_args:
-			params+='-'+param['name']+' '+str(param['value'])+' '
-	else:
-		print(f"Nom de script manquant de le fichier de configuration pour l'action : {action_name}",action_name)
-		return -1
-
-	for host in hosts:
-		try :
-			if host['name']=='local':
-				print(f'./scripts/{script_name} {params}')
-				#os.system(f'./scripts/{script_name} {params}')
-			else :
-				##get all the
-				print("else")
-
-		except Exception as ex :
-			print(ex)
-			return -1
-
-	return 1
-
-
-def wait_module(action):
-
-  name=action['name']
-  wait_time=action['time']
-  input_msg=action['input']
-  text=action['text']
-
-  if (wait_time):
-    print(text)
-    time.sleep(wait_time)
-
-  if (input_msg):
-    for interaction in input_msg:
-      input(interaction["name"])
-
-  return 1
-
-
-def hook_module(action):
-	name=action['name']
-	route=action['route']
-
-	Hook(name,route)
-
-	return 1
-
-
-args=sys.argv
-
-activity_name= args[1]
-path='./activite/'+activity_name
-
-config_file=open(path)
-
-parsed_config_file=yaml.load(config_file,Loader=yaml.FullLoader)
-
-actions = parsed_config_file['actions']
-
-for action in actions:
-
+for action in data['actions']:
 	if(action['type']=='script'):
-		script_module(action)
+		actions.append(Script(action))
 
 	elif(action['type']=='wait'):
-		wait_module(action)
+		actions.append(Wait(action))
 
 	elif(action['type']=='hook'):
-		hook_module(action)
+		actions.append(Hook(action))
 
 	else:
 		raise Exception("Action must have a type")
 
-
+while len(actions) > 1 :
+	action = actions.pop(1)
+	action.run()
